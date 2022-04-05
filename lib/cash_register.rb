d8de1fc373c3fe43283721fe1bb8add1ecb38649
Jacob Yaw <yawjacob@yahoo.com>
2022-04-04 20:15:28 -0500

    commit tree 878428ebf618febb873f2fa555db2b7e8222b479

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+require 'pry'
 
+class CashRegister
+    
+    attr_accessor :total, :discount
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = 20
+    end
+
+    def add_item(title, price, quantity=1)
+        self.total += price * quantity
+    end
+
+end
